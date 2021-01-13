de5bc18b713bf3a86a3d622a6cb87887b1dbe5ce
stam <stam@epicm.org>
2021-01-14 01:06:48 +0300

    target platforms fixed

--- src/atlassian-downloader/build.sh
+++ src/atlassian-downloader/build.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
 dotnet publish -r win7-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
 dotnet publish -r win7-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r win81-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r win81-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r win10-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r win10-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r linux-x64 --self-contained true  --framework net5.0  --configuration Release 
-dotnet publish -r osx-x64   --self-contained true  --framework net5.0  --configuration Release 
-dotnet publish -r linux-musl-x64 --self-contained true  --framework net5.0  --configuration Release 
+dotnet publish -r win81-arm --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet publish -r win10-arm64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet publish -r linux-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
+dotnet publish -r linux-musl-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
+dotnet publish -r osx-x64   --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
