2a98e13a2635e869f440c1db16470b6dfd8f3f95
stam <stam@epicm.org>
2021-01-13 15:27:33 +0300

    v1.0.0.1

--- src/Program.cs
+++ src/Program.cs
@@ -4,43 +4,76 @@ using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 
-var output = "output";
-var feedUrl = "https://my.atlassian.com/download/feeds/archived/jira.json";
-    //https://my.atlassian.com/download/feeds/current/jira-software.json
+var outputDir = "output";
+var feedUrls =
+    new[] {
+        "https://my.atlassian.com/download/feeds/archived/bamboo.json",
+        "https://my.atlassian.com/download/feeds/archived/confluence.json",
+        "https://my.atlassian.com/download/feeds/archived/crowd.json",
+        "https://my.atlassian.com/download/feeds/archived/crucible.json",
+        "https://my.atlassian.com/download/feeds/archived/fisheye.json",
+        "https://my.atlassian.com/download/feeds/archived/jira-core.json",
+        "https://my.atlassian.com/download/feeds/archived/jira-servicedesk.json",
+        "https://my.atlassian.com/download/feeds/archived/jira-software.json",
+        "https://my.atlassian.com/download/feeds/archived/jira.json",
+        "https://my.atlassian.com/download/feeds/archived/stash.json",
+
+        "https://my.atlassian.com/download/feeds/current/bamboo.json",
+        "https://my.atlassian.com/download/feeds/current/confluence.json",
+        "https://my.atlassian.com/download/feeds/current/crowd.json",
+        "https://my.atlassian.com/download/feeds/current/crucible.json",
+        "https://my.atlassian.com/download/feeds/current/fisheye.json",
+        "https://my.atlassian.com/download/feeds/current/jira-core.json",
+        "https://my.atlassian.com/download/feeds/current/jira-servicedesk.json",
+        "https://my.atlassian.com/download/feeds/current/jira-software.json",
+        "https://my.atlassian.com/download/feeds/current/stash.json"
+    };
+
+
 
 var client = new HttpClient();
-var atlassianJson = await client.GetStringAsync(feedUrl);
-var callString = "downloads(";
-var json = atlassianJson[callString.Length..^1];
-var parsed = JsonSerializer.Deserialize<ResponseArray[]>(json, new JsonSerializerOptions
+foreach (var feedUrl in feedUrls)
 {
-    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-});
-var versions = parsed.GroupBy(a => a.Version).ToDictionary(a => a.Key, a => a.Select(b => b.ZipUrl).ToArray());
+    var feedDir = Path.Combine(outputDir, feedUrl[(feedUrl.LastIndexOf('/') + 1)..(feedUrl.LastIndexOf('.'))]);
+    var atlassianJson = await client.GetStringAsync(feedUrl);
+    var callString = "downloads(";
+    var json = atlassianJson[callString.Length..^1];
+    var parsed = JsonSerializer.Deserialize<ResponseArray[]>(json, new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    });
+    var versions = parsed.GroupBy(a => a.Version).ToDictionary(a => a.Key, a => a.ToArray());
 
-foreach (var version in versions)
-{
-    var directory = Path.Combine(output, version.Key);
-    if (!Directory.Exists(directory))
+    foreach (var version in versions)
     {
-        Directory.CreateDirectory(directory);
-    }
-    foreach (var link in version.Value)
-    {
-        var q = link.PathAndQuery;
-        var outputFile = Path.Combine(directory, q[(q.LastIndexOf("/") + 1)..]);
-        if (!File.Exists(outputFile))
+        var directory = Path.Combine(feedDir, version.Key);
+        if (!Directory.Exists(directory))
         {
-            using var file = File.OpenWrite(outputFile);
-            using var request = await client.GetStreamAsync(link).ConfigureAwait(false);
-            await request.CopyToAsync(file).ConfigureAwait(false);
-            Console.WriteLine($"Downloaded {link}");
+            Directory.CreateDirectory(directory);
         }
-        else
+        foreach (var file in version.Value)
         {
-            Console.WriteLine($"File for {link} already exists");
+            var serverPath = file.ZipUrl.PathAndQuery;
+            var outputFile = Path.Combine(directory, serverPath[(serverPath.LastIndexOf("/") + 1)..]);
+            if (!File.Exists(outputFile))
+            {
+                if (!string.IsNullOrEmpty(file.Md5))
+                {
+                    File.WriteAllText(outputFile + ".md5", file.Md5);
+                }
+                using var outputStream = File.OpenWrite(outputFile);
+                using var request = await client.GetStreamAsync(file.ZipUrl).ConfigureAwait(false);
+                await request.CopyToAsync(outputStream).ConfigureAwait(false);
+                Console.WriteLine($"Downloaded {outputFile}");
+            }
+            else
+            {
+                Console.WriteLine($"File for {file.ZipUrl} already exists");
+            }
         }
     }
+    Console.WriteLine($"Downloaded all files from " +
+        $"{feedUrl}");
 }
 Console.WriteLine("Download complete");
 
