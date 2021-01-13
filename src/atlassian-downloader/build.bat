157aba2acea3a50c2546a5ad72fc22d10f236f78
STAM <developer@epicm.org>
2021-01-14 01:29:52 +0300

    bump

--- src/atlassian-downloader/build.bat
+++ src/atlassian-downloader/build.bat
@@ -1,3 +1,6 @@
+SET DOTNET_CLI_TELEMETRY_OPTOUT=true
+SET DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+
 dotnet.exe publish -r win7-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
 dotnet.exe publish -r win7-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
 dotnet.exe publish -r win81-arm --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
--- src/atlassian-downloader/build.sh
+++ src/atlassian-downloader/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-dotnet publish -r win7-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r win7-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r win81-arm --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
-dotnet publish -r win10-arm64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+export DOTNET_CLI_TELEMETRY_OPTOUT=true
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+
+dotnet publish -r win7-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=false
+dotnet publish -r win7-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=false
+dotnet publish -r win81-arm --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=false
+dotnet publish -r win10-arm64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=false
 dotnet publish -r linux-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
 dotnet publish -r linux-musl-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
 dotnet publish -r osx-x64   --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
