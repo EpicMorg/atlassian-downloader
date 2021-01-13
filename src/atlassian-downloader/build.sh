96d18a46e95515dfaa2d3cab8fb3d3bd87d0f17c
stam <stam@epicm.org>
2021-01-14 00:50:40 +0300

    bump

--- src/atlassian-downloader/build.sh
+++ src/atlassian-downloader/build.sh
@@ -5,4 +5,5 @@ dotnet publish -r win81-x86 --self-contained true  --framework net5.0  --configu
 dotnet publish -r win10-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
 dotnet publish -r win10-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
 dotnet publish -r linux-x64 --self-contained true  --framework net5.0  --configuration Release 
+dotnet publish -r osx-x64   --self-contained true  --framework net5.0  --configuration Release 
 dotnet publish -r linux-musl-x64 --self-contained true  --framework net5.0  --configuration Release 
