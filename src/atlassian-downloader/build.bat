efcbb7df921d1c3effa444dd5ee60c4e9e015db1
stam <stam@epicm.org>
2021-01-13 20:50:03 +0300

    build fixes

--- src/atlassian-downloader/build.bat
+++ src/atlassian-downloader/build.bat
@@ -1,4 +1,8 @@
-dotnet.exe publish -r win7-x64 --self-contained true  --framework net5.0  --configuration Release
-dotnet.exe publish -r win7-x86 --self-contained true  --framework net5.0  --configuration Release
-dotnet.exe publish -r linux-x64 --self-contained true  --framework net5.0  --configuration Release
-dotnet.exe publish -r linux-musl-x64 --self-contained true  --framework net5.0  --configuration Release
+dotnet.exe publish -r win7-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet.exe publish -r win7-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet.exe publish -r win81-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet.exe publish -r win81-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet.exe publish -r win10-x64 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet.exe publish -r win10-x86 --self-contained true  --framework net5.0  --configuration Release -p:PublishTrimmed=true -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet.exe publish -r linux-x64 --self-contained true  --framework net5.0  --configuration Release 
+dotnet.exe publish -r linux-musl-x64 --self-contained true  --framework net5.0  --configuration Release 
