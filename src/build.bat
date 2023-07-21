d39833ac9dc4f496a0bb474cfb33fd8d6eb6549c
STAM <stam@epicm.org>
2023-07-21 12:32:50 +0300

    build scripts improvments

--- src/build.bat
+++ src/build.bat
@@ -8,3 +8,29 @@ dotnet.exe publish --runtime win10-arm64 --force --self-contained true  --framew
 dotnet.exe publish --runtime linux-x64 --force --self-contained true  --framework net6.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
 dotnet.exe publish --runtime linux-musl-x64 --force --self-contained true  --framework net6.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
 dotnet.exe publish --runtime osx-x64 --force --self-contained true  --framework net6.0  --configuration Release -p:PublishTrimmed=false -p:PublishSingleFile=false -p:PublishReadyToRun=false
+
+del /F bin\\Release\\net6.0\\linux-musl-x64\\publish\\atlassian-downloader.pdb
+del /F bin\\Release\\net6.0\\linux-x64\\publish\\atlassian-downloader.pdb
+del /F bin\\Release\\net6.0\\osx-x64\\publish\\atlassian-downloader.pdb
+del /F bin\\Release\\net6.0\\win10-arm64\\publish\\atlassian-downloader.pdb
+del /F bin\\Release\\net6.0\\win7-x64\\publish\\atlassian-downloader.pdb
+del /F bin\\Release\\net6.0\\win7-x86\\publish\\atlassian-downloader.pdb
+del /F bin\\Release\\net6.0\\win81-arm\\publish\\atlassian-downloader.pdb
+
+type nul > bin/Release/net6.0/linux-musl-x64/publish/createdump.exe.ignore
+type nul > bin/Release/net6.0/linux-x64/publish/createdump.exe.ignore
+type nul > bin/Release/net6.0/osx-x64/publish/createdump.exe.ignore
+type nul > bin/Release/net6.0/win10-arm64/publish/createdump.exe.ignore
+type nul > bin/Release/net6.0/win7-x64/publish/createdump.exe.ignore
+type nul > bin/Release/net6.0/win7-x86/publish/createdump.exe.ignore
+type nul > bin/Release/net6.0/win81-arm/publish/createdump.exe.ignore
+
+7z a -tzip -mx5 -r0 ./bin/atlassian-downloader-net6.0-linux-musl-x64.zip ./bin/Release/net6.0/linux-musl-x64/publish/*
+7z a -tzip -mx5 -r0 ./bin/atlassian-downloader-net6.0-linux-x64.zip ./bin/Release/net6.0/linux-x64/publish/*
+7z a -tzip -mx5 -r0 ./bin/atlassian-downloader-net6.0-osx-x64.zip ./bin/Release/net6.0/osx-x64/publish/*
+7z a -tzip -mx5 -r0 ./bin/atlassian-downloader-net6.0-win10-arm64.zip ./bin/Release/net6.0/win10-arm64/publish/*
+7z a -tzip -mx5 -r0 ./bin/atlassian-downloader-net6.0-win7-x64.zip ./bin/Release/net6.0/win7-x64/publish/*
+7z a -tzip -mx5 -r0 ./bin/atlassian-downloader-net6.0-win7-x86.zip ./bin/Release/net6.0/win7-x86/publish/*
+7z a -tzip -mx5 -r0 ./bin/atlassian-downloader-net6.0-win81-arm.zip ./bin/Release/net6.0/win81-arm/publish/*
+
+
