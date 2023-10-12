00000a2119b2fc56c908608253dca4ab33934e2a
Konstantin Safonov <kasthack@epicm.org>
2023-10-12 23:33:26 +0300

    Fix #36: restore args parsing. Move bells and whistles from downloader service Apply code styles. Fix exception when parsing json Move sources to a separate type

---
+++ src/Models/SourceInformation.cs
@@ -0,0 +1,85 @@
+﻿namespace EpicMorg.Atlassian.Downloader.Models;
+using System.Collections.Generic;
+
+internal static class SourceInformation
+{
+    public static IReadOnlyList<string> AtlassianSources { get; } = new[] {
+
+            //official links
+            "https://my.atlassian.com/download/feeds/archived/bamboo.json",
+            "https://my.atlassian.com/download/feeds/archived/clover.json",
+            "https://my.atlassian.com/download/feeds/archived/confluence.json",
+            "https://my.atlassian.com/download/feeds/archived/crowd.json",
+            "https://my.atlassian.com/download/feeds/archived/crucible.json",
+            "https://my.atlassian.com/download/feeds/archived/fisheye.json",
+            "https://my.atlassian.com/download/feeds/archived/jira-core.json",
+            "https://my.atlassian.com/download/feeds/archived/jira-servicedesk.json",
+            "https://my.atlassian.com/download/feeds/archived/jira-software.json",
+            "https://my.atlassian.com/download/feeds/archived/jira.json",
+            "https://my.atlassian.com/download/feeds/archived/stash.json",
+            "https://my.atlassian.com/download/feeds/archived/mesh.json",
+
+            //cdn mirror of official links
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/bamboo.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/clover.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/confluence.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/crowd.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/crucible.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/fisheye.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/jira-core.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/jira-servicedesk.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/jira-software.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/jira.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/stash.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/mesh.json",
+
+            //official links
+            "https://my.atlassian.com/download/feeds/current/bamboo.json",
+            "https://my.atlassian.com/download/feeds/current/clover.json",
+            "https://my.atlassian.com/download/feeds/current/confluence.json",
+            "https://my.atlassian.com/download/feeds/current/crowd.json",
+            "https://my.atlassian.com/download/feeds/current/crucible.json",
+            "https://my.atlassian.com/download/feeds/current/fisheye.json",
+            "https://my.atlassian.com/download/feeds/current/jira-core.json",
+            "https://my.atlassian.com/download/feeds/current/jira-servicedesk.json",
+            "https://my.atlassian.com/download/feeds/current/jira-software.json",
+            "https://my.atlassian.com/download/feeds/current/stash.json",
+            "https://my.atlassian.com/download/feeds/current/mesh.json",
+
+            //cdn mirror of official links
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/bamboo.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/clover.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/confluence.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/crowd.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/crucible.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/fisheye.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/jira-core.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/jira-servicedesk.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/jira-software.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/stash.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/mesh.json",
+
+            //official links
+            "https://my.atlassian.com/download/feeds/eap/bamboo.json",
+            "https://my.atlassian.com/download/feeds/eap/confluence.json",
+            "https://my.atlassian.com/download/feeds/eap/jira.json",
+            "https://my.atlassian.com/download/feeds/eap/jira-servicedesk.json",
+            "https://my.atlassian.com/download/feeds/eap/stash.json",
+            //"https://my.atlassian.com/download/feeds/eap/mesh.json", //404
+
+            //cdn mirror of official links
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/eap/bamboo.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/eap/confluence.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/eap/jira.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/eap/jira-servicedesk.json",
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/eap/stash.json",
+            //"https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/eap/mesh.json",  //404
+            
+            //https://raw.githubusercontent.com/EpicMorg/atlassian-json/master/json-backups/archived/sourcetree.json //unstable link with r\l
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/archived/sourcetree.json",
+            
+            //https://raw.githubusercontent.com/EpicMorg/atlassian-json/master/json-backups/current/sourcetree.json //unstable link with r\l
+            "https://raw.githack.com/EpicMorg/atlassian-json/master/json-backups/current/sourcetree.json"
+
+        };
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿namespace EpicMorg.Atlassian.Downloader;
+
+using EpicMorg.Atlassian.Downloader.Core;
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -6,47 +10,42 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 
 using System;
-using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 
-namespace EpicMorg.Atlassian.Downloader
+public class Program
 {
-    public class Program
-    {
-        /// <summary>
-        /// Atlassian archive downloader. See https://github.com/EpicMorg/atlassian-downloader for more info
-        /// </summary>
-        /// <param name="Action">Action to perform</param>
-        /// <param name="OutputDir">Override output directory to download</param>
-        /// <param name="customFeed">Override URIs to import</param>
-        /// <param name="Version">Show credits banner</param>
-        /// <param name="productVersion">Override target version to download some product. Advice: Use it with "customFeed".</param>
-        static async Task Main(string OutputDir, Uri[] customFeed = null, DownloadAction Action = DownloadAction.Download, bool Version = false, string productVersion = null) => await
-            Host
-                .CreateDefaultBuilder()
-                .ConfigureHostConfiguration(configHost => configHost.AddEnvironmentVariables())
-                .ConfigureAppConfiguration((ctx, configuration) =>
-                    configuration
-                        .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
-                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                        .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                        .AddEnvironmentVariables())
-                .ConfigureServices((ctx, services) => services
-                       .AddOptions()
-                       .AddLogging(builder =>
-                       {
-                            Log.Logger = new LoggerConfiguration()
-                                   .ReadFrom.Configuration(ctx.Configuration)
-                                   .CreateLogger();
-                            builder
-                                .ClearProviders()
-                                .AddSerilog(dispose: true);
-                       })
-                       .AddHostedService<DonloaderService>()
-                       .AddSingleton(new DownloaderOptions(OutputDir, customFeed, Action, Version, productVersion))
-                       .AddHttpClient())
-                .RunConsoleAsync()
-                .ConfigureAwait(false);
-    }
+    /// <summary>
+    /// Atlassian archive downloader. See https://github.com/EpicMorg/atlassian-downloader for more info
+    /// </summary>
+    /// <param name="action">Action to perform</param>
+    /// <param name="outputDir">Override output directory to download</param>
+    /// <param name="customFeed">Override URIs to import</param>
+    /// <param name="about">Show credits banner</param>
+    /// <param name="productVersion">Override target version to download some product. Advice: Use it with "customFeed".</param>
+    static async Task Main(string outputDir, Uri[] customFeed = null, DownloadAction action = DownloadAction.Download, bool about = false, string productVersion = null) => await
+        Host
+            .CreateDefaultBuilder()
+            .ConfigureHostConfiguration(configHost => configHost.AddEnvironmentVariables())
+            .ConfigureAppConfiguration((ctx, configuration) =>
+                configuration
+                    .SetBasePath(System.AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables())
+            .ConfigureServices((ctx, services) => services
+                   .AddOptions()
+                   .AddLogging(builder =>
+                   {
+                       Log.Logger = new LoggerConfiguration()
+                              .ReadFrom.Configuration(ctx.Configuration)
+                              .CreateLogger();
+                       _ = builder
+                            .ClearProviders()
+                            .AddSerilog(dispose: true);
+                   })
+                   .AddHostedService<DownloaderService>()
+                   .AddSingleton(new DownloaderOptions(outputDir, customFeed, action, about, productVersion))
+                   .AddHttpClient())
+            .RunConsoleAsync()
+            .ConfigureAwait(false);
 }
